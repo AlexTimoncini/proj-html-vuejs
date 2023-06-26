a2accf8ffbc23a442a193511c637c82d06a12a05
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-27 00:16:47 +0200

    added carousle to process section with vue feature

--- src/components/MainProcess.vue
+++ src/components/MainProcess.vue
@@ -1,6 +1,54 @@
 <script>
+    import 'vue3-carousel/dist/carousel.css'
+    import { Carousel, Slide } from 'vue3-carousel';
+    import { ref } from 'vue';
+    const myCarousel = ref(null);
     export default {
         name: 'MainProcess',
+        components: {
+            Carousel,
+            Slide,
+        },
+        data() {
+            return {
+                processList: [
+                    {
+                        name: 'Scripting',
+                        description: "We'll take your idea and create a technical script which consists of action notes and animation descriptions",
+                        img: '../assets/process1.png'
+                    },
+                    {
+                        name: 'Pre-Production',
+                        description: "We'll take your idea and create a technical script which consists of action notes and animation descriptions",
+                        img: '../assets/process2.png'
+                    },
+                    {
+                        name: 'Scripting',
+                        description: "We'll take your idea and create a technical script which consists of action notes and animation descriptions",
+                        img: '../assets/process1.png'
+                    },
+                    {
+                        name: 'Pre-Production',
+                        description: "We'll take your idea and create a technical script which consists of action notes and animation descriptions",
+                        img: '../assets/process2.png'
+                    },
+                ],
+            }
+        },
+        mounted() {
+            this.myCarousel = this.$refs.myCarousel;
+        },
+        methods: {
+            prev(){
+                this.myCarousel.prev()
+            },
+            next(){
+                this.myCarousel.next()
+            },
+            getImagePath: function(imgPath) {
+                return new URL(imgPath, import.meta.url).href;
+            }
+        }
     }
 </script>
 
@@ -14,10 +62,37 @@
                 <div class="ivy_small_container">
                     <h2 class="ivy_slogan">Our Process</h2>
                     <h1 class="ivy_title">Our Process for Your Animation Production</h1>
-                    <p>We have an effective process for working on animation</p>
+                    <p class="ivy_desc">We have an effective process for working on animation</p>
+                    <Carousel
+                    ref="myCarousel"
+                    :items-to-show="2" 
+                    :wrap-around="true" 
+                    :autoplay="2000" 
+                    :pauseAutoplayOnHover="true" 
+                    :snapAlign="'start'">
+                        <Slide v-for="slide, index in processList" :key="slide">
+                            <div class="carousel__item">
+                                <div class="ivy_icon_box">
+                                    <img :src="getImagePath(slide.img)" :alt="slide.name">
+                                </div>
+                                <h1>{{ slide.name }}</h1>
+                                <p>{{ slide.description }}</p>
+                                <div class="ivy_index_banner">
+                                    {{ index + 1 }}
+                                </div>
+                            </div>
+                        </Slide>
+                    </Carousel>
+                    <div class="ivy_carousel_nav">
+                        <button @click="prev">
+                            <i class="fa-solid fa-arrow-left"></i>
+                        </button>
+                        <button @click="next">
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </button>
+                    </div>
                 </div>
             </div>
-
         </div>
     </section>
 </template>
@@ -39,6 +114,11 @@
                     left: 0;
                     top: 0;
                     transform: translateX(-50%);
+                    transition: all 0.2s linear;
+
+                    &:hover{
+                        transform: translateX(-47%);
+                    }
                 }
             }
             .ivy_right_side{
@@ -55,8 +135,91 @@
                     font-size: 3.5rem;
                     padding-bottom: 1rem;
                 }
+                .ivy_desc{
+                    padding-bottom: 2rem
+                }
+
+                .carousel__item {
+                    @include flex(column, center, center, nowrap);
+                    height: 100%;
+                    padding: 2rem;
+                    border: 2px solid $grey;
+                    color: $purple;
+                    font-size: 20px;
+                    border-radius: 20px;
+                    position: relative;
+                    .ivy_icon_box{
+                        width: 140px;
+                        height: 140px;
+                        border-radius: 50%;
+                        background-color:#FFF5F0;
+                        display: flex;
+
+                        img{
+                            object-fit: contain;
+                            display: block;
+                            margin: auto;
+                        }
+                    }
+
+                    h1{
+                        margin: 1rem 0;
+                        font-size: 1.5rem;
+                    }
+
+                    .ivy_index_banner{
+                        position: absolute;
+                        display: flex;
+                        align-items: flex-end;
+                        background-color: $grey;
+                        top: 0;
+                        right: 10%;
+                        padding: 0 1rem 1rem;
+                        border-bottom-left-radius: 1rem;
+                        border-bottom-right-radius: 1rem;
+                        height: 100px;
+                        color: $darkGrey;
+                    }
+                }
+            
+                .carousel__slide {
+                    padding: 10px;
+                }
+            
+                .carousel__prev,
+                .carousel__next {
+                    box-sizing: content-box;
+                    border: 5px solid transparent;
+                }
+
+                .ivy_carousel_nav{
+                    display: flex;
+                    column-gap: 10px;
+                    margin-top: 10px;
+                    button{
+                        @include flex();
+                        border: 1px solid $darkGrey;
+                        border-radius: 50%;
+                        color: $darkGrey;
+                        padding: 1rem;
+                        font-size: 1.3rem;
+                        background: none;
+                        width: 50px;
+                        height: 50px;
+                        cursor: pointer;
+                        transition: all 0.2s linear;
+
+                        &:hover{
+                            background-color: $orange;
+                            border-color: $orange;
+                            color: $white;
+                        }
+                    }
+                }
             }
         }
     }
+
+
 
 </style>

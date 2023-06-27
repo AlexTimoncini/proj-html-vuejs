5ec33582dc7cdeffdbdac17e9b609f9889f07943
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-27 10:23:09 +0200

    added style to email form in main tips section

--- src/components/MainTips.vue
+++ src/components/MainTips.vue
@@ -21,6 +21,7 @@
                         amount: '12+'
                     },
                 ],
+                userMail: '',
             }
         },
     }
@@ -29,17 +30,15 @@
 <template>
     <section id="yourTips">
         <div class="container_size">
-        <div class="ivy_small_container">
-            <h2>We Create New Worlds!</h2>
-            <h1>Want Tips On Your Storytelling?</h1>
-            <p>Join our newsletter for tips on how to elevate your brand through storytelling</p>         
-        </div>
-
-
-            <div class="ivy_email">
-                <input type="email" placeholder="Enter you email">
-                <button type="submit">Submit</button>
+            <div class="ivy_small_container">
+                <h2>We Create New Worlds!</h2>
+                <h1>Want Tips On Your Storytelling?</h1>
+                <p>Join our newsletter for tips on how to elevate your brand through storytelling</p>         
             </div>
+            <form class="ivy_email">
+                <input type="email" placeholder="Enter you email" v-model="userMail">
+                <button type="submit" @click.prevent="userMail = ''">Submit</button>
+            </form>
         </div>
     </section>
 </template>
@@ -53,7 +52,6 @@
         color: $white;
         padding: 100px 0;
         text-align: center;
-
         .ivy_small_container{
             width: 60%;
             margin: 0 auto;
@@ -66,6 +64,30 @@
             h2{
                 color: $orange;
                 padding-bottom: 1.5rem;
+            }
+        }
+
+        .ivy_email{
+            width: 50%;
+            margin: 0 auto;
+            background-color: $white;
+            padding: 1rem;
+            border-radius: 40px;
+            display: flex;
+
+            input{
+                flex-grow: 1;
+                border: none;
+                outline: none;
+                font-size: 1rem;
+            }
+
+            button{
+                border: none;
+                background-color: $pink;
+                padding: 0.9rem 1.9rem;
+                border-radius: 20px;
+                font-size: 1.2rem;
             }
         }
 

ab330d72ef0a72de8daef7ab3dbd68b078cfd0d7
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-27 07:43:56 +0200

    added yellow rocket bg img in main process

--- src/components/MainProcess.vue
+++ src/components/MainProcess.vue
@@ -93,6 +93,9 @@
                     </div>
                 </div>
             </div>
+            <!--BACKGROUND IMAGES-->
+            <img src="../assets/smart4.png" alt="yellow rocket" class="ivy_rocket">
+
         </div>
     </section>
 </template>
@@ -101,11 +104,10 @@
     @use '../styles/partials/variables' as *;
     @use '../styles/partials/mixin' as *;
     #ourProcess{
-        padding: 100px 0;
-
+        padding: 100px 0 200px;
+        position: relative;
         .container_size{
             display: flex;
-
             .ivy_left_side{
                 width: 50%;
                 position: relative;
@@ -123,99 +125,106 @@
             }
             .ivy_right_side{
                 width: 50%;
+                .ivy_small_container{
+                    width: 100%;
+                    margin: 0 auto;
+                    .ivy_slogan{
+                        color: $blue;
+                        padding-bottom: 1.5rem;
+                    }
+                    .ivy_title{
+                        font-size: 3.5rem;
+                        padding-bottom: 1rem;
+                    }
+                    .ivy_desc{
+                        padding-bottom: 2rem
+                    }
+    
+                    .carousel__item {
+                        @include flex(column, center, center, nowrap);
+                        height: 100%;
+                        padding: 2rem;
+                        border: 2px solid $grey;
+                        color: $purple;
+                        font-size: 20px;
+                        border-radius: 20px;
+                        position: relative;
+                        .ivy_icon_box{
+                            width: 140px;
+                            height: 140px;
+                            border-radius: 50%;
+                            background-color:#FFF5F0;
+                            display: flex;
+    
+                            img{
+                                object-fit: contain;
+                                display: block;
+                                margin: auto;
+                            }
+                        }
+    
+                        h1{
+                            margin: 1rem 0;
+                            font-size: 1.5rem;
+                        }
+    
+                        .ivy_index_banner{
+                            position: absolute;
+                            display: flex;
+                            align-items: flex-end;
+                            background-color: $grey;
+                            top: 0;
+                            right: 10%;
+                            padding: 0 1rem 1rem;
+                            border-bottom-left-radius: 1rem;
+                            border-bottom-right-radius: 1rem;
+                            height: 100px;
+                            color: $darkGrey;
+                        }
+                    }
+                
+                    .carousel__slide {
+                        padding: 10px;
+                    }
+                
+                    .carousel__prev,
+                    .carousel__next {
+                        box-sizing: content-box;
+                        border: 5px solid transparent;
+                    }
+    
+                    .ivy_carousel_nav{
+                        display: flex;
+                        column-gap: 10px;
+                        margin-top: 10px;
+                        button{
+                            @include flex();
+                            border: 1px solid $darkGrey;
+                            border-radius: 50%;
+                            color: $darkGrey;
+                            padding: 1rem;
+                            font-size: 1.3rem;
+                            background: none;
+                            width: 50px;
+                            height: 50px;
+                            cursor: pointer;
+                            transition: all 0.2s linear;
+    
+                            &:hover{
+                                background-color: $orange;
+                                border-color: $orange;
+                                color: $white;
+                            }
+                        }
+                    }
+                }
             }
-            .ivy_small_container{
-                width: 100%;
-                margin: 0 auto;
-                .ivy_slogan{
-                    color: $blue;
-                    padding-bottom: 1.5rem;
-                }
-                .ivy_title{
-                    font-size: 3.5rem;
-                    padding-bottom: 1rem;
-                }
-                .ivy_desc{
-                    padding-bottom: 2rem
-                }
-
-                .carousel__item {
-                    @include flex(column, center, center, nowrap);
-                    height: 100%;
-                    padding: 2rem;
-                    border: 2px solid $grey;
-                    color: $purple;
-                    font-size: 20px;
-                    border-radius: 20px;
-                    position: relative;
-                    .ivy_icon_box{
-                        width: 140px;
-                        height: 140px;
-                        border-radius: 50%;
-                        background-color:#FFF5F0;
-                        display: flex;
-
-                        img{
-                            object-fit: contain;
-                            display: block;
-                            margin: auto;
-                        }
-                    }
-
-                    h1{
-                        margin: 1rem 0;
-                        font-size: 1.5rem;
-                    }
-
-                    .ivy_index_banner{
-                        position: absolute;
-                        display: flex;
-                        align-items: flex-end;
-                        background-color: $grey;
-                        top: 0;
-                        right: 10%;
-                        padding: 0 1rem 1rem;
-                        border-bottom-left-radius: 1rem;
-                        border-bottom-right-radius: 1rem;
-                        height: 100px;
-                        color: $darkGrey;
-                    }
-                }
-            
-                .carousel__slide {
-                    padding: 10px;
-                }
-            
-                .carousel__prev,
-                .carousel__next {
-                    box-sizing: content-box;
-                    border: 5px solid transparent;
-                }
-
-                .ivy_carousel_nav{
-                    display: flex;
-                    column-gap: 10px;
-                    margin-top: 10px;
-                    button{
-                        @include flex();
-                        border: 1px solid $darkGrey;
-                        border-radius: 50%;
-                        color: $darkGrey;
-                        padding: 1rem;
-                        font-size: 1.3rem;
-                        background: none;
-                        width: 50px;
-                        height: 50px;
-                        cursor: pointer;
-                        transition: all 0.2s linear;
-
-                        &:hover{
-                            background-color: $orange;
-                            border-color: $orange;
-                            color: $white;
-                        }
-                    }
-                }
+            .ivy_rocket{
+            position: absolute;
+            right: 5px;
+            bottom: 5px;
+            height: 200px;
+            z-index: -1;
             }
         }
     }

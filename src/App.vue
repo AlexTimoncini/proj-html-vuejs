c02e57eb12544ef064d6fa2c6586bb54a1cfb362
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-27 15:32:18 +0200

    added loader that appears for a second after page is loaded, (still not visible before)

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,46 @@
 import HeaderApp from './components/HeaderApp.vue';
 import MainApp from './components/MainApp.vue';
 import FooterApp from './components/FooterApp.vue';
+import AppLoader from './components/AppLoader.vue';
   export default {
     name: 'App',
     components: {
       HeaderApp,
       MainApp,
       FooterApp,
+      AppLoader
+    }, 
+    data() {
+    return {
+      showLoader: true,
+    };
+  },
+  mounted() {
+    this.loadingComplete();
+  },
+  methods: {
+    loadingComplete(){
+      window.addEventListener('load', () => {
+      console.log('FATTO AMMO');
+      setTimeout(()=>{
+        this.showLoader = false;
+      }, 1000);
+    });
     }
+  }
   }
 </script>
 
 <template>
-    <HeaderApp />
-    <MainApp />
-    <FooterApp />
+    <div class="app" :class="showLoader ? 'hidden' : ''">
+      <HeaderApp />
+      <MainApp />
+      <FooterApp />
+    </div>
+    <AppLoader :class="showLoader ? '' : 'hidden'"/>
 </template>
 
 <style lang="scss">
   @import url('./styles/general.scss');
+
 </style>
---
+++ src/components/AppLoader.vue
@@ -0,0 +1,43 @@
+<script>
+    export default{
+        name: 'AppLoader'
+    }
+</script>
+
+<template>
+    <div class="wrapper">
+        <div class="img_box">
+            <img src="../assets/rocket-banner.png" alt="">
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .wrapper{
+        background: url(../assets/preloader.png);
+        display: flex;
+        background-size: cover;
+        height: 100vh;
+        width: 100%;
+
+        .img_box{
+            margin: auto;
+            animation: slide 3s linear infinite;
+            img{
+                display: block;
+            }
+            @keyframes slide {
+                0%{
+                    transform: translateX(-100px);
+                }
+                50%{
+                    transform: translateX(100px);
+                    
+                }
+                100%{
+                    transform: translateX(-100px);
+                }
+            }
+        }
+    }
+</style>

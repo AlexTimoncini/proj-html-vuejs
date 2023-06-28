b143bfd76c0aca6b037ff1338fba35201c457dc7
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-28 11:40:56 +0200

    fixed border transition on card section

--- src/components/MainCardSection.vue
+++ src/components/MainCardSection.vue
@@ -11,7 +11,7 @@
                         entry.target.classList.add('spawn');
                         observer.unobserve(entry.target);
                     });
-                });
+                },{threshold: 0});
                 const cards = document.querySelectorAll('.ivy_card')
                 cards.forEach(card => observer.observe(card));
             }
@@ -29,25 +29,31 @@
             </div>
             <div class="ivy_cards">
                 <div class="ivy_card">
-                    <img src="../assets/smart1.png" alt="smart icon" loading="lazy">
-                    <h3 class="ivy_card_title">3D modeling</h3>
-                    <p class="ivy_card_desc">
-                        We constantly ask questions. It helps us shape your story and deliver the results you crave. Yep, we'll drill deep to deliver above and beyond your expectations.
-                    </p>
+                    <div class="wrapper">
+                        <img src="../assets/smart1.png" alt="smart icon" loading="lazy">
+                        <h3 class="ivy_card_title">3D modeling</h3>
+                        <p class="ivy_card_desc">
+                            We constantly ask questions. It helps us shape your story and deliver the results you crave. Yep, we'll drill deep to deliver above and beyond your expectations.
+                        </p>
+                    </div>
                 </div>
                 <div class="ivy_card">
-                    <img src="../assets/smart2.png" alt="smart icon" loading="lazy">
-                    <h3 class="ivy_card_title">Collaboration</h3>
-                    <p class="ivy_card_desc">
-                        We constantly ask questions. It helps us shape your story and deliver the results you crave. Yep, we'll drill deep to deliver above and beyond your expectations.
-                    </p>
+                    <div class="wrapper">
+                        <img src="../assets/smart2.png" alt="smart icon" loading="lazy">
+                        <h3 class="ivy_card_title">Collaboration</h3>
+                        <p class="ivy_card_desc">
+                            We constantly ask questions. It helps us shape your story and deliver the results you crave. Yep, we'll drill deep to deliver above and beyond your expectations.
+                        </p>
+                    </div>
                 </div>
                 <div class="ivy_card">
-                    <img src="../assets/smart3.png" alt="smart icon" loading="lazy">
-                    <h3 class="ivy_card_title">Creativity</h3>
-                    <p class="ivy_card_desc">
-                        We constantly ask questions. It helps us shape your story and deliver the results you crave. Yep, we'll drill deep to deliver above and beyond your expectations.
-                    </p>
+                    <div class="wrapper">
+                        <img src="../assets/smart3.png" alt="smart icon" loading="lazy">
+                        <h3 class="ivy_card_title">Creativity</h3>
+                        <p class="ivy_card_desc">
+                            We constantly ask questions. It helps us shape your story and deliver the results you crave. Yep, we'll drill deep to deliver above and beyond your expectations.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -87,55 +93,63 @@
 
             .ivy_card{
                 width: calc(100% / 3 - 2rem);
-                padding: 2.5rem 1rem;
-                background-color: antiquewhite;
                 border-radius: 35px;
-                border: 1px solid rgb(178,181,210);
                 transform: translateY(20px);
                 opacity: 0;
-                &:hover{
-                    border: 1px solid transparent;
-                }
-                img{
-                    margin: 0 auto;
-                }
-                .ivy_card_title{
-                    font-size: 1.8rem;
-                    margin: 1rem 0;
+                transition: all 1s linear;
+                .wrapper{
+                    border: 1px solid rgb(178,181,210);
+                    padding: 2.5rem 1rem;
+                    width: 100%;
+                    border-radius: 35px;
+                    transition: all 0.2s linear;
+                    img{
+                        margin: 0 auto;
+                    }
+                    .ivy_card_title{
+                        font-size: 1.8rem;
+                        margin: 1rem 0;
+                    }
+                    .ivy_card_desc{
+                        font-size: 1.1rem;
+                    }
+
+                    &:hover{
+                        border: 1px solid transparent;
+                    }
                 }
 
-                .ivy_card_desc{
-                    font-size: 1.1rem;
-                }
             }
 
             .ivy_card:nth-child(1){
-                background-image: linear-gradient(#ffffff, #FFF2EA);
-                transition: transform 1s linear 200ms;
-                transition: opacity 1s linear 200ms;
-                &:hover{
-                    background-image: linear-gradient(#FFF2EA,#ffffff );
+                transition-delay: 200ms;
+                .wrapper{
+                    background: linear-gradient(#ffffff, #FFF2EA);
+                }
+                &:hover .wrapper{
+                    background: linear-gradient(#FFF2EA,#ffffff );
                 }
             }
 
             .ivy_card:nth-child(2){
-                background-image: linear-gradient(#ffffff, #EEECFC);
-                transition: transform 1s linear 400ms;
-                transition: opacity 1s linear 400ms;
-                &:hover{
-                    background-image: linear-gradient(#EEECFC,#ffffff );
+                transition-delay: 400ms;
+                .wrapper{
+                    background: linear-gradient(#ffffff, #EEECFC);
+                }
+                &:hover .wrapper{
+                    background: linear-gradient(#EEECFC,#ffffff );
                 }
             }
 
             .ivy_card:nth-child(3){
-                background-image: linear-gradient(#ffffff, #FFE7E8);
-                transition: transform 1s linear 600ms;
-                transition: opacity 1s linear 600ms;
+                transition-delay: 600ms;
+                .wrapper{
+                    background: linear-gradient(#ffffff, #FFE7E8);
+                }
                 &:hover{
-                    background-image: linear-gradient(#FFE7E8,#ffffff );
+                    background: linear-gradient(#FFE7E8,#ffffff );
                 }
             }
-
             .ivy_card.spawn{
                 @include slide_end();
             }

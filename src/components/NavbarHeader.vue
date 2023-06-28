5012aa8d3c2bda5fda2e9909bc29f871dd55fda2
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-06-28 14:23:49 +0200

    added responsive behaviour to navbar

--- src/components/NavbarHeader.vue
+++ src/components/NavbarHeader.vue
@@ -35,22 +35,33 @@
                 ],
             }
         },
+        methods: {
+            displayMenu(){
+                const ulMenu = document.querySelector('.ivy_nav_menu')
+                ulMenu.classList.toggle('visible')
+            }
+        },
     }
 </script>
 
 <template>
-    <nav class="container_size">
-        <div class="ivy_logo_box">
-            <img src="../assets/logo.png" alt="Anidio Logo" loading="lazy">
+    <nav>
+        <div class="container_size">
+            <div class="ivy_logo_box">
+                <img src="../assets/logo.png" alt="Anidio Logo" loading="lazy">
+            </div>
+            <ul class="ivy_nav_menu">
+                <li class="ivy_nav_item" v-for="link in navLinks">
+                    <a :href="link.link">{{ link.title }}</a>
+                </li>
+                <li class="pink_btn">
+                    <a href="#">Get A Quote</a>
+                </li>
+            </ul>
+            <button @click="displayMenu" class="ivy_nav_toggle">
+                <i class="fa-solid fa-bars-staggered"></i>
+            </button>
         </div>
-        <ul class="ivy_nav_menu">
-            <li class="ivy_nav_item" v-for="link in navLinks">
-                <a :href="link.link">{{ link.title }}</a>
-            </li>
-            <li class="pink_btn">
-                <a href="#">Get A Quote</a>
-            </li>
-        </ul>
     </nav>
 </template>
 
@@ -59,34 +70,74 @@
     @use '../styles/partials/variables' as *;
 
     nav{
-        @include flex(row, space-between, center, nowrap);
-        padding: 1rem 0;
-
-        .ivy_nav_menu{
-            @include flex();
-
-            .ivy_nav_item{
-                margin: 0 0.6rem;
-
-                a{
-                    color: $white;
-                    font-weight: bold;
-                    font-size: 1.2rem;
+        padding: 1rem 0.2rem;
+        position: relative;
+        .container_size{
+            @include flex(row, space-between, center, nowrap);
+            .ivy_nav_menu{
+                @include flex();
+                .ivy_nav_item{
+                    margin: 0 0.6rem;
+    
+                    a{
+                        color: $white;
+                        font-weight: bold;
+                        font-size: 1.2rem;
+                    }
+                }
+    
+                .pink_btn{
+                    padding: 0.9rem 1.9rem;
+                    background-color: $pink;
+                    border-radius: 35px;
+                    margin-left: 0.6rem;
+    
+                    a{
+                        color: $purple;
+                        font-weight: bold;
+                        font-size: 1.2rem;
+                    }
                 }
             }
 
-            .pink_btn{
-                padding: 0.9rem 1.9rem;
-                background-color: $pink;
-                border-radius: 35px;
-                margin-left: 0.6rem;
-
-                a{
-                    color: $purple;
-                    font-weight: bold;
-                    font-size: 1.2rem;
-                }
+            .ivy_nav_toggle{
+                border: none;
+                background: none;
+                color: $white;
+                display: none;
+                font-size: 1.6rem;
+                padding-right: 0.3rem;
+                cursor: pointer;
             }
+        }
+    }
+
+    /**MEDIA QUERIES**/
+    @media only screen and (max-width: 992px){
+
+        
+        nav .container_size .ivy_nav_menu{
+            @include flex(column, center, center, nowrap);
+            position: absolute;
+            top: 100%;
+            left: 0;
+            width: 100%;
+            background-color: $purple;
+            z-index: 1;
+            transform: translateY(-50px);
+            opacity: 0;
+            transition: all 0.5s ease;
+        }
+
+        nav .container_size .ivy_nav_menu.visible{
+            height: auto;
+            opacity: 1;
+            transform: translateY(0px);
+        }
+
+        nav .container_size .ivy_nav_toggle{
+            display: block;
+            z-index: 2;
         }
     }
 </style>
